0a005f569c5821fd30b06c34a52c877842fc5f7f
VibeAndFlew <teasetqwertyuiop@gmail.com>
2023-10-14 21:18:46 +0530

    footer-button-test
    #0002

--- components/Footer.js
+++ components/Footer.js
@@ -1,11 +1,18 @@
-import styles from './Footer.module.css'
+import styles from './Footer.module.css';
 
 export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        This is a test <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        Made with ❤️ , Mottu <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <iframe
+          width="110"
+          height="200"
+          src="https://www.myinstants.com/instant/8-bit-happy-birthday-62739/embed/"
+          frameBorder="0"
+          scrolling="no"
+        ></iframe>
       </footer>
     </>
-  )
+  );
 }
